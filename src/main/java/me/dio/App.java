8ae678e579ba5bdbcc09dedafa31553f0b53f63d
layinl <layon.final008@gmail.com>
2023-10-09 09:46:55 -0300

    feat: Modified output from App.java

--- src/main/java/me/dio/App.java
+++ src/main/java/me/dio/App.java
@@ -1,12 +1,10 @@
 package me.dio;
 
 /**
- * Hello world!
- *
+ * App
  */
 public class App {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        System.out.println("Hello! If you're reading this, I'm working just fine");
     }
 }
